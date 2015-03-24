dd06b93142769f1b8bfb2055289eec7e1656deab
Tery Lim <kumabotz@gmail.com>
2015-03-24 13:31:38 +1300

    Open REST route

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 mongoose.connect('mongodb://localhost/news');
 
