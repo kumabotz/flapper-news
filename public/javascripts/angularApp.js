c84558d4ecd72e06335ff2dfc637b53e82260012
Tery Lim <kumabotz@gmail.com>
2015-03-24 13:50:43 +1300

    Wiring posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -6,7 +6,12 @@
       .state('home', {
         url: '/home',
         templateUrl: '/home.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['posts', function(posts) {
+            return posts.getAll();
+          }]
+        }
       })
       .state('posts', {
         url: '/posts/{id}',
@@ -16,35 +21,43 @@
     $urlRouterProvider.otherwise('home');
   }]);
 
-  app.factory('posts', [function() {
-    var o = {
-      posts: [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4, comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]}
-      ]
+  app.factory('posts', ['$http', function($http) {
+    var o = { posts: [] };
+
+    o.getAll = function() {
+      return $http.get('/posts').success(function(data) {
+        angular.copy(data, o.posts);
+      });
     };
+
+    o.create = function(post) {
+      return $http.post('/posts', post).success(function(data) {
+        o.posts.push(data);
+      });
+    };
+
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post._id + '/upvote')
+        .success(function(data) {
+          post.upvotes += 1;
+        });
+    };
+
     return o;
   }]);
 
   app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
     $scope.posts = posts.posts;
     $scope.addPost = function() {
-      $scope.posts.push({
+      posts.create({
         title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
+        link: $scope.link
       });
       $scope.title = '';
       $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+      posts.upvote(post);
     };
   }]);
 
