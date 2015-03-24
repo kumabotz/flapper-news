bc879750dfeb6502394f485a7ddc0f9f1412a120
Tery Lim <kumabotz@gmail.com>
2015-03-24 13:41:58 +1300

    Fix index

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts) {
     if (err) return next(err);
